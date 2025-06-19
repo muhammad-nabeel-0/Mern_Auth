2cb72d06d4a848651cc27d0499db9e6fe44b44f6
muhammad-nabeel-0 <nabeelgaming786yt@gmail.com>
2025-06-19 18:20:57 +0500

    app backend live url

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ connectDB();
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://your-frontend.vercel.app'], // update this
+  origin: ['http://localhost:5173', 'https://mern-auth-git-main-nabeel-munirs-projects.vercel.app'], // update this
   credentials: true,
 }));
 
