a3ce2e1cd50a5b2c6c7c8faec7deef5f1c1e425a
muhammad-nabeel-0 <nabeelgaming786yt@gmail.com>
2025-06-19 19:43:51 +0500

    try to fix it

--- server/controller/auth.controller.js
+++ server/controller/auth.controller.js
@@ -32,12 +32,13 @@ const register = async (req,res)=>{
         const user = new userModel({name,email,password:hashPassword});
         await user.save();
         const token = jwt.sign({id:user._id},process.env.JWT_SECRET,{expiresIn:"7d"})
-        res.cookie('token',token,{
-            httpOnly:true,
-            maxAge: 7 * 24 * 60 * 60 * 1000,
-            secure: process.env.NODE_ENV === "production",
-            sameSite:process.env.NODE_ENV === "production" ? "None" : "Lax"
-        })
+        res.cookie('token', token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "none",
+        maxAge: 7 * 24 * 60 * 60 * 1000
+    });
+
         // sending welcome email 
 
         const emailOptions = {
@@ -88,12 +89,12 @@ const login = async (req,res)=>{
         }
 
         const token =   jwt.sign({id:user._id},process.env.JWT_SECRET,{expiresIn:"7d"})
-        res.cookie('token',token,{
-            httpOnly:true,
-            maxAge: 7 * 24 * 60 * 60 * 1000,
-            secure: process.env.NODE_ENV === "production",
-            sameSite:process.env.NODE_ENV === "production" ? "None" : "Lax"
-        })
+        res.cookie('token', token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "none",
+        maxAge: 7 * 24 * 60 * 60 * 1000
+    });
         return res.json({success:true})
         
     } catch (error) {
@@ -111,8 +112,8 @@ const logout = async (req,res)=>{
     try {
         res.clearCookie("token",{
             httpOnly:true,
-            secure: process.env.NODE_ENV === "production",
-            sameSite:process.env.NODE_ENV === "production" ? "none" : "strict"
+            secure: true,
+            sameSite:"none"
         })
         return res.json({
             success:true,
--- server/server.js
+++ server/server.js
@@ -15,8 +15,7 @@ connectDB();
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-  origin: ['http://localhost:5173',
-    'https://mern-auth-git-main-nabeel-munirs-projects.vercel.app'], // update this
+  origin: ['https://mern-auth-git-main-nabeel-munirs-projects.vercel.app'], // update this
   credentials: true
 }));
 
