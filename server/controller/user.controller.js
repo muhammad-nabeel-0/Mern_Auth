6feb095580843843f38e286f35ee2b89c3ce54d5
muhammad-nabeel-0 <nabeelgaming786yt@gmail.com>
2025-06-20 11:29:54 +0500

    Fix user ID reference in getUserData function

--- server/controller/user.controller.js
+++ server/controller/user.controller.js
@@ -3,7 +3,7 @@ import userModel from "../models/user.model.js";
 
 export const getUserData = async(req,res)=>{
     try {
-        const userId = req.user._id
+        const userId = req.user.id
         const user = await userModel.findById(userId)
         
         
